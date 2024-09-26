2457964ce6b9bc4bc3cd02f9f576d73295d5d367
@lepicka.klaudia <klaudia@revdebug.com>
2024-09-26 12:50:19 +0200

    Invoice App v1

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <AppHeader /> <!-- Poprawiona nazwa komponentu -->
+    <div class="container content">
+      <router-view></router-view>
+    </div>
+    <AppFooter /> <!-- Poprawiona nazwa komponentu -->
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/Header.vue'; // Import poprawionego komponentu
+import AppFooter from './components/Footer.vue'; // Import poprawionego komponentu
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader, // Upewnij się, że komponent jest używany
+    AppFooter  // Upewnij się, że komponent jest używany
+  }
+}
+</script>
+
+<style src="./assets/App.css"></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <p>© 2024 - RevDeBug</p>
+        </div>
+        <div class="col-lg-6"></div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter' // Zmieniono nazwę komponentu
+};
+</script>
